b78b609847966d7c84cbe24973afd589e3ec38d7
D1rtyJesus <artemtsurkana@gmail.com>
2026-03-27 00:36:58 +0200

    test version of algoritm to destruct usless cache details

--- ОП другий семестр/lab3/memory.js
+++ ОП другий семестр/lab3/memory.js
@@ -1,1 +1,22 @@
-function memory(fn, options = {}) {}
+
+if (cache.size >= maxSize) {
+    
+    if (strategy === "LRU") {
+        const oldestKey = cache.keys().next().value;
+        cache.delete(oldestKey);
+        
+    } else if (strategy === "LFU") {
+        let minCount = Infinity;
+        let lfuKey = null;
+        
+        for (const [key, record] of cache) {
+            if (record.count < minCount) {
+                minCount = record.count;
+                lfuKey = key;
+            }
+        }
+        cache.delete(lfuKey);
+    }
+}
+
+cache.set(key, { value: result, count: 1 });
