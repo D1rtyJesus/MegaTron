55399e5fd39fb8b89cae395600577832982537de
D1rtyJesus <artemtsurkana@gmail.com>
2026-05-06 23:00:21 +0300

    lab 4 improvment

--- ОП другий семестр/lab 4/grunt.js
+++ ОП другий семестр/lab 4/grunt.js
@@ -1,1 +1,28 @@
-nothing ever happens
+class Pque {
+  constructor() {
+    this.queue = [];
+  }
+
+  enter(item, priority) {
+    const newE = { item, priority };
+    this.queue.push(newElement);
+    exit(type); {
+      if (type === 'oldest') {
+        return this.queue.shift();
+      }
+      if (type === 'newest') {
+        return this.queue.pop();
+      }
+      if (type === 'highest') {
+      let highestIndex = 0; 
+      let highestPriority = this.queue[0].priority; 
+
+      for (let i = 0; i < this.queue.length; i++) {
+
+      }
+      
+я
+    }
+    }
+  }
+}
