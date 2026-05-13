8d95968ee7068d53b31535edc6c211868562b539
D1rtyJesus <artemtsurkana@gmail.com>
2026-05-13 20:42:38 +0300

    function 1 export version

--- ОП другий семестр/lab 5/main.js
+++ ОП другий семестр/lab 5/main.js
@@ -1,6 +1,32 @@
-const playlist = [
-  'Lose Yourself',
-  'Yellow Submarine',
-  'Sweet Dreams',
-  'Radioactive'
-];
+
+function mapAsyncCallback(array, iteratee, finalCallback) {
+    if (array.length === 0) {
+        return finalCallback(null, []);
+    }
+
+    const results = [];
+    let completedCount = 0;
+    let hasErrored = false;
+
+    array.forEach((item, index) => {
+        iteratee(item, index, (err, mappedItem) => {
+            if (hasErrored) return; 
+
+            if (err) {
+                hasErrored = true;
+                return finalCallback(err);
+            }
+
+            results[index] = mappedItem; 
+            completedCount++;
+
+            if (completedCount === array.length) {
+                finalCallback(null, results);
+            }
+        });
+    });
+}
+
+module.exports = {
+    mapAsyncCallback,
+};
